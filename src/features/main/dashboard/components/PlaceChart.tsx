8eedecc3d17f54fb6dfe021657feaf7f5bb801c3
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-04 22:18:50 +0900

    Feat : 대시보드 플레이스 추적 슬롯 컴포넌트

--- src/features/main/dashboard/components/PlaceChart.tsx
+++ src/features/main/dashboard/components/PlaceChart.tsx
@@ -1,17 +1,10 @@
 import React from "react";
+import PanelTitle from "../../common/PanelTitle";
 
 export default function PlaceChart() {
   return (
-    <div
-      className="col-span-2"
-      style={{
-        minHeight: 470,
-        minWidth: 700,
-        background: "#FFFFFF",
-        borderRadius: "6px",
-      }}
-    >
-      PlaceChart
+    <div className="col-span-2 min-h-[470px] min-w-[700px] bg-white rounded-md p-5 shadow-md">
+      <PanelTitle title="플레이스 추적 슬롯" />
     </div>
   );
 }
