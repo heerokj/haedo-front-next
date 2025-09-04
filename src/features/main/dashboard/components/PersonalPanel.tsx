6a6cdfc83134268f61ec1550a761ea091f31ec34
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-04 22:19:09 +0900

    Feat : 대시보드 공지사항 컴포넌트

--- src/features/main/dashboard/components/PersonalPanel.tsx
+++ src/features/main/dashboard/components/PersonalPanel.tsx
@@ -1,16 +1,14 @@
 import React from "react";
+import PanelTitle from "../../common/PanelTitle";
 
 export default function PersonalPanel() {
   return (
-    <div
-      style={{
-        minHeight: 470,
-        minWidth: 350,
-        background: "#FFFFFF",
-        borderRadius: "6px",
-      }}
-    >
-      PersonalPanel
+    <div className="min-h-[470px] min-w-[350px] bg-white rounded-md p-5 shadow-md">
+      <PanelTitle title="개인알림" />
+      {/* 구현 예정 */}
+      <p className="text-[#212529] border-b border-[#DEE2E6] py-2.5 text-[12px]">
+        아직 알람이 없습니다.
+      </p>
     </div>
   );
 }
