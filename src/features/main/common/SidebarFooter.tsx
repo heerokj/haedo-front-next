4f74163e2f7ac229af773186a55728a1630669e3
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-04 22:20:02 +0900

    Feat : 사이드바 푸터 컴포넌트

--- src/features/main/common/SidebarFooter.tsx
+++ src/features/main/common/SidebarFooter.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function SidebarFooter() {
-  return <div>SidebarFooter</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="text-[#626F86] text-[10px] leading-5">
+        <p className="text-[15px]">해도기획</p>
+        <p>대표자 : 정호진</p>
+        <p>사업자 등록번호 : 618-13-30057</p>
+        <p>경기도 의정부시 둔야로 33번길 14</p>
+        <p>개인정보관리책임자 : 권재호</p>
+        <p>haedo_company@naver.com</p>
+      </div>
+    </div>
+  );
 }
